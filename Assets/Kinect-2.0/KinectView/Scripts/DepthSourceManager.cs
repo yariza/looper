7523cd4561d59fa713e80ed788f0ae21895c195b
Yujin Ariza <yujin.ariza@gmail.com>
2019-04-09 18:47:56 -0400

    import kinect

---
+++ Assets/Kinect-2.0/KinectView/Scripts/DepthSourceManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+using System;
+
+public class DepthSourceManager : GameSingleton<DepthSourceManager>
+{
+    [System.Serializable]
+    public enum UpdateModel
+    {
+        Polling,
+        Event,
+    };
+    public UpdateModel _UpdateModel = UpdateModel.Polling;
+
+    public delegate void DepthFrameArrivedCallback(ushort[] data);
+    public event DepthFrameArrivedCallback OnDepthFrameArrived;
+
+    private KinectSensor _Sensor;
+    private DepthFrameReader _Reader;
+    private ushort[] _Data;
+
+    public ushort[] GetData()
+    {
+        return _Data;
+    }
+
+    void Start () 
+    {
+        try
+        {
+            _Sensor = KinectSensor.GetDefault();
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Kinect dll not installed.");
+        }
+
+        if (_Sensor != null) 
+        {
+            _Reader = _Sensor.DepthFrameSource.OpenReader();
+            _Data = new ushort[_Sensor.DepthFrameSource.FrameDescription.LengthInPixels];
+
+            if (_UpdateModel == UpdateModel.Event)
+            {
+                _Reader.FrameArrived += _OnFrameArrived;
+            }
+
+            if (!_Sensor.IsOpen)
+            {
+                _Sensor.Open();
+            }
+        }
+    }
+
+    private void _OnFrameArrived(object sender, DepthFrameArrivedEventArgs e)
+    {
+        var frame = _Reader.AcquireLatestFrame();
+        if (frame != null)
+        {
+            frame.CopyFrameDataToArray(_Data);
+            frame.Dispose();
+            frame = null;
+        }
+        if (OnDepthFrameArrived != null)
+        {
+            OnDepthFrameArrived(_Data);
+        }
+    }
+
+    void Update ()
+    {
+        if (_UpdateModel != UpdateModel.Polling) return;
+
+        if (_Reader != null)
+        {
+            var frame = _Reader.AcquireLatestFrame();
+            if (frame != null)
+            {
+                frame.CopyFrameDataToArray(_Data);
+                frame.Dispose();
+                frame = null;
+            }
+        }
+    }
+    
+    void OnApplicationQuit()
+    {
+        if (_Reader != null)
+        {
+            _Reader.Dispose();
+
+            if (_UpdateModel == UpdateModel.Event)
+            {
+                _Reader.FrameArrived -= _OnFrameArrived;
+            }
+            _Reader = null;
+        }
+
+        if (_Sensor != null)
+        {
+            if (_Sensor.IsOpen)
+            {
+                _Sensor.Close();
+            }
+            
+            _Sensor = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Common/DeleteAfterSeconds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DeleteAfterSeconds : NetworkBehaviour {
+
+    [SerializeField]
+    float _seconds = 1f;
+
+	// Use this for initialization
+	void Start () {
+        if (!isServer) return;
+        StartCoroutine(WaitForSeconds());
+	}
+
+    IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(_seconds);
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
