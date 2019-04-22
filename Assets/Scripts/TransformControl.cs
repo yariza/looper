6ca58fdfcd98bc3b4a5ca8bb0d531b84f81405d4
Yujin Ariza <yujin.ariza@gmail.com>
2019-04-22 11:07:01 -0700

    transform control + studio setup

---
+++ Assets/Scripts/TransformControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformControl : MonoBehaviour
+{
+    #region Serialized fields
+
+    [SerializeField, Range(0, 1)]
+    float _speed = 0.1f;
+
+    [SerializeField, Range(0, 90)]
+    float _rotationSpeed = 20f;
+
+    #endregion
+
+    #region Unity events
+
+    private void Update()
+    {
+        Vector3 dPos = Vector3.zero;
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            dPos.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            dPos.x += 1f;
+        }
+        if (Input.GetKey(KeyCode.Keypad5))
+        {
+            dPos.z -= 1f;
+        }
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            dPos.z += 1f;
+        }
+        if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            dPos.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.KeypadPlus))
+        {
+            dPos.y += 1f;
+        }
+        dPos *= _speed * Time.deltaTime;
+
+        transform.localPosition += dPos;
+
+        Vector3 dEuler = Vector3.zero;
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            dEuler.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.Keypad3))
+        {
+            dEuler.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.Keypad7))
+        {
+            dEuler.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.Keypad9))
+        {
+            dEuler.x += 1f;
+        }
+        dEuler *= _rotationSpeed * Time.deltaTime;
+
+        transform.localEulerAngles += dEuler;
+    }
+        
+    #endregion
+}
